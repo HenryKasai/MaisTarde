f7c9c669aa1dda50cc6bbb28c30624424c811e33
HenryKasai <henrykasai1775@gmail.com>
2020-05-29 20:02:42 -0300

    Exercício Resolvido.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MaisTarde
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime horaatual = DateTime.Now;
+            string horaatualstring = horaatual.ToShortTimeString();
+            DateTime acréscimo = horaatual.AddHours(6).AddMinutes(30);
+            string acréscimostring = acréscimo.ToShortTimeString();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Agora é exatamente {horaatualstring}, mais tarde será {acréscimostring}.");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+        }
+    }
+}
